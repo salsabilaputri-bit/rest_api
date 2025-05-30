9ba7ab0427f5ec41b2637bec231c5aab17bb807c
Your Name <you@example.com>
2025-05-30 14:12:03 +0700

    membuar cards dan memanggil data poster movies

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -10,9 +10,28 @@ $('#search-button').on('click', function () {
         },
         success: function (result) {
            if (result.Response == "True") {
-           
+            let movies = result.Search;
+            
+            $.each(movies, function (i,data){
+                $('#movie-list').append(`
+                    <div class="card">
+                    <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>   
+            `);
+            });
+
+
             } else {
-                $('#movie-list').html('<h1 class="text-center">' + result.Error +'</h1>')
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">` + result.Error +`</h1>
+                    </div>
+                `)
             }
         }
     });
