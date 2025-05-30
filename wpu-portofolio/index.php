d7df43c76bb6a41897959577b16ac5f9556b2fa3
Your Name <you@example.com>
2025-05-31 00:40:52 +0700

    berhasil menampilkan data dari youtube dan menampilkan button subscribe

--- wpu-portofolio/index.php
+++ wpu-portofolio/index.php
@@ -1,3 +1,30 @@
+<?php
+function get_CURL($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec($curl);
+  curl_close($curl);
+  
+  return json_decode($result, true);
+}
+
+$result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCcOTQHfcdKdbornwL8qlemA&key=AIzaSyCRxWs-COjDPN48bARcZawPmT1OTcVF9fA');
+
+
+
+$youtubeProfilePic = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+$channelName = $result['items'][0]['snippet']['title'];
+$subscriber = $result['items'][0]['statistics']['subscriberCount'];
+
+//latest video
+$urlLatestVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCRxWs-COjDPN48bARcZawPmT1OTcVF9fA&channelId=UCcOTQHfcdKdbornwL8qlemA&maxResults=1&order=date&part=snippet';
+$result = get_CURL($urlLatestVideo);
+$latestVideoId = $result['items'][0]['id']['videoId'];
+
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,7 +44,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#home">Sandhika Galih</a>
+        <a class="navbar-brand" href="#home">Salsabila Putri</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -42,8 +69,8 @@
       <div class="container">
         <div class="text-center">
           <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
-          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
+          <h1 class="display-4">Salsabila Putri</h1>
+          <h3 class="lead">IT Student</h3>
         </div>
       </div>
     </div>
@@ -81,28 +108,55 @@
             <div class="col-md-5">
               <div class="row">
                 <div class="col-md-4">
-                 <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail"> 
+                 <img src="<?= $youtubeProfilePic; ?>" width="200" class="rounded-circle 
+                 img-thumbnail"> 
                 </div>
                 <div class="col-md-8">
-                  <h5>WebProgrammingUNPAS</h5>
-                  <p>951000 Subsribers</p>
+                  <h5><?= $channelName; ?></h5>
+                  <p><?= $subscriber; ?> Subscribers</p>
+                  <div class="g-ytsubscribe" data-channelid="UCcOTQHfcdKdbornwL8qlemA" data-layout="default"data-count="default"></div>
                 </div>
               </div>
               <div class="row mt-3 pb-3">
                 <div class="col">
                 <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" 
-                src="https://www.youtube.com/embed/ye1FKPufftw?rel=0" 
+                src="https://www.youtube.com/embed/<?= $latestVideoId; ?>" 
                 allowfullscreen></iframe>
                 </div>
-
                 </div>
               </div>
             </div>
-            <div class="col-md-5"></div>
+
+            <div class="col-md-5">
+              <div class="row">
+                <div class="col-md-4">
+                  <img src="img/profile1.png" width="200" class="rounded-circle 
+                  img-thumbnail">
+                </div>
+                <div class="col-md-8">
+                  <h5>@salsabilaputri00_</h5>
+                  <p>2 Followers</p>
+                </div>
+              </div>
+
+              <div class="row mt-3 pb-3">
+                <div class="col">
+                  <div class="ig-thumbnail">
+                    <img src="img/thumbs/1.png">
+                  </div>
+                  <div class="ig-thumbnail">
+                    <img src="img/thumbs/2.png">
+                  </div>
+                  <div class="ig-thumbnail">
+                    <img src="img/thumbs/3.png">
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-     </section>
+      </section>
 
 
     <!-- Portfolio -->
@@ -242,15 +296,11 @@
     </footer>
 
 
-
-
-
-
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
